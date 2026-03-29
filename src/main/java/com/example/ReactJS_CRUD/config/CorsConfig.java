1169d4450da05ebe391383fba5b329170a652c56
Vaibhav Rawade <vaibhavrawade123@gmail.com>
2026-03-29 17:33:06 +0530

    Done-ComposeFile3sm

--- src/main/java/com/example/ReactJS_CRUD/config/CorsConfig.java
+++ src/main/java/com/example/ReactJS_CRUD/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//vaibhav
+//vaibhav aa
         registry.addMapping("/**")
         		.allowedOrigins(
         				"http://localhost:3000",
