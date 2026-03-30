208395fc5529844cbb3d636581221a5681522b3e
Vaibhav Rawade <vaibhavrawade123@gmail.com>
2026-03-30 16:55:12 +0530

    Done-ComposeFile3smsxxzzzajsjxdx

--- src/main/java/com/example/ReactJS_CRUD/ReactJsCrudApplication.java
+++ src/main/java/com/example/ReactJS_CRUD/ReactJsCrudApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class ReactJsCrudApplication {
 	
-	int a,b,c;
+	int a=10,b,c;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactJsCrudApplication.class, args);
